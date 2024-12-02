9e15d68c2277ab2a162e3762a72bc9214ca83186
Aleksei Smyshliaev <imperial.frame@yandex.ru>
2024-12-02 03:29:39 +0300

    review: Исправлены замечания

--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -11,8 +11,8 @@ import java.util.Map;
 public class InMemoryHistoryManager implements HistoryManager {
 
     static class HistoryList {
-        private Node<Task> head;
-        private Node<Task> tail;
+        private final Node<Task> head = new Node<>(null, null, this.tail);
+        private final Node<Task> tail = new Node<>(this.head, null, null);
         private final Map<Long, Node<Task>> nodeHistory = new HashMap<>();
 
         public void linkLast(Task element) {
@@ -20,52 +20,34 @@ public class InMemoryHistoryManager implements HistoryManager {
                 removeNode(element.getId());
             }
 
-            final Node<Task> oldTail = tail;
-            final Node<Task> newNode = new Node<>(oldTail, element, null);
-            tail = newNode;
-            if (oldTail == null) {
-                head = newNode;
-            } else
-                oldTail.setNext(newNode);
+            final Node<Task> newNode = new Node<>(this.tail.getPrev(), element, this.tail);
+            this.tail.getPrev().setNext(newNode);
+            this.tail.setPrev(newNode);
 
             nodeHistory.put(element.getId(), newNode);
         }
 
         public void removeNode(long nodeId) {
-            if (nodeHistory.containsKey(nodeId)) {
-                Node<Task> deletedNode = nodeHistory.get(nodeId);
-                final Node<Task> oldPrev = deletedNode.getPrev();
-                final Node<Task> oldNext = deletedNode.getNext();
-
-                if (oldPrev != null) {
-                    if (oldNext != null) {
-                        oldPrev.setNext(deletedNode.getNext());
-                        oldNext.setPrev(deletedNode.getPrev());
-                    } else {
-                        oldPrev.setNext(null);
-                        this.tail = oldPrev;
-                    }
-                } else {
-                    if (oldNext != null) {
-                        oldNext.setPrev(null);
-                        this.head = oldNext;
-                    } else {
-                        this.head = null;
-                        this.tail = null;
-                    }
-                }
-                nodeHistory.remove(nodeId);
+            if (!nodeHistory.containsKey(nodeId)) {
+                return;
             }
+
+            Node<Task> deletedNode = nodeHistory.get(nodeId);
+            final Node<Task> oldPrev = deletedNode.getPrev();
+            final Node<Task> oldNext = deletedNode.getNext();
+
+            oldPrev.setNext(oldNext);
+            oldNext.setPrev(oldPrev);
+
+            nodeHistory.remove(nodeId);
         }
 
         public List<Task> getTasks() {
             List<Task> tasks = new ArrayList<>();
-            Node<Task> currentNode = this.head;
-            if (currentNode != null) {
-                do {
-                    tasks.add(currentNode.getData());
-                    currentNode = currentNode.getNext();
-                } while (currentNode != null);
+            Node<Task> currentNode = this.head.getNext();
+            while (currentNode.getData() != null) {
+                tasks.add(currentNode.getData());
+                currentNode = currentNode.getNext();
             }
             return tasks;
         }
